92d833bba138f3e99fc31e784622a2f6e5152781
Guilherme Saravi <gsaravi@psgtecnologia.com.br>
2024-03-03 19:13:40 -0400

    Dividindo app em Widgets e implementando nova transacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './components/transaction_user.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -18,24 +17,6 @@ class ExpensesApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  createExpense() {
-    print(titleController.text);
-    print(valueController.text);
-  }
-
-  final _transactions = [
-    Transaction(
-        id: 't1',
-        title: 'Novo Tênis de Corrida',
-        value: 310.76,
-        date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Conta de Luz', value: 211.30, date: DateTime.now()),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,71 +27,18 @@ class MyHomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        Container(
-            width: double.infinity,
-            child: const Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('Gráfico'),
-            )),
-        Column(
-            children: _transactions.map((tr) {
-          return Card(
-              child: Row(
-            children: [
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'R\$${tr.value.toStringAsFixed(2)}',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tr.title,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  Text(DateFormat('d MMM y').format(tr.date),
-                      style: TextStyle(color: Colors.grey[600]))
-                ],
-              )
-            ],
-          ));
-        }).toList()),
-        Card(
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                TextField(
-                  controller: titleController,
-                  decoration: const InputDecoration(labelText: 'Título'),
-                ),
-                TextField(
-                    controller: valueController,
-                    decoration: const InputDecoration(labelText: 'Valor (R\$)')),
-                TextButton(
-                  onPressed: createExpense,
-                  style: TextButton.styleFrom(foregroundColor: Colors.purple),
-                  child: const Text('Nova Transação'),
-                )
-              ],
-            ),
-          ),
-        ),
-      ]),
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Container(
+              width: double.infinity,
+              child: const Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
+              )),
+          TransactionUser(),
+        ]),
+      ),
     );
   }
 }
